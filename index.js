9fc624cd0c37a49137d381e901bbdc454a7f4c9c
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-06-23 14:09:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, word) {
+  let message = []
+  for (let i = 0; i < array.length; i++) {
+    message.push(`Thank you, ${array[i]}, for the wonderful ${word} gift!`);
+  }
+  return message;
+}
+
+function countdown(num) {
+  for (let i = num; i >= 1; i--) {
+    console.log(num);
+    num-=1;
+  }
+  console.log(num);
+}
